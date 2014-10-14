6ccb92befe67e73ae57033ce6795bd45caf0312a
huntinggirled <huntinggirled@gmail.com>
2014-10-14 17:08:38 +0900

    Update.

--- let/turnover.js
+++ let/turnover.js
@@ -1,22 +1,36 @@
 (function(){
-	var ua=[],ul=ulist.length,ll=0;
-	for(var i=ul;i--;){
-		var tb=ulist[i].number-1000;
-		if(null!=tb&&0<tb&&1000>tb){
-			if(0==ll){
-				ll=tb;
+	ua=[];
+	ul=ulist.length;
+	ll=0;
+	yl=0;
+	yc=0;
+	es=20;
+	log='';
+	for(i=ul;i--;){
+		tb=ulist[i].number;
+		if(null!=tb&&tb<2000){
+			if(tb<1000) {
+				tb*=1;
+				if(0==yl) yl=tb;
+				yc++;
+			} else {
+				tb-=1000;
 			}
-			bs=Math.floor(tb/50);
-			if(null==ua[bs]){
-				ua[bs]=0;
-			}
+			if(0==ll) ll=tb;
+			bs=Math.floor((tb-1)/es);
+			if(null==ua[bs]) ua[bs]=0;
 			ua[bs]+=1;
 		}
 	}
-	for(var j=ua.length;j--;){
-		var n=(50-ua[j])/50*100;
-		console.log(50*j+"から"+(50*(j+1)-1)+"までの離職率: "+n.toFixed(2)+"%");
+	for(j=ua.length;j--;){
+		em=((es*(j+1))>ll)?(ll-es*j):es;
+		n=(em-ua[j])/em*100;
+		log+=((es*j)+1)+"から"+((es*j)+em)+"までの離職率: "+n.toFixed(2)+"%"+"\n";
 	}
-	var t=(ll-ul)/ll*100;
-	console.log("全体の離職率: "+t.toFixed(2)+"%");
+	t=(ll-ul)/ll*100;
+	log+="全体の離職率: "+t.toFixed(2)+"%"+"\n";
+	t=(yl-yc)/yl*100;
+	log+="役員の離職率: "+t.toFixed(2)+"%"+"\n";
+	console.log(log);
+	alert(log);
 })();
